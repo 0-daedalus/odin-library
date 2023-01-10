52e391212776a782d0d5a4ae835b4621bf0b01be
0-daedalus <108616390+0-daedalus@users.noreply.github.com>
2023-01-10 11:54:44 +0600

    Set up some JS

---
+++ script.js
@@ -0,0 +1,21 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isFinished){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isFinished = isFinished;
+}
+
+function storeBook(book){
+    myLibrary.push(book);
+}
+
+function displayBooks(){
+    myLibrary.forEach(book => {
+        let bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "card");
+        let content = document.querySelector(".content");
+        content.appendChild(bookCard);
+    });
+}
